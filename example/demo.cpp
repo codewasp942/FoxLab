b0920100c944467f8ebf13dd695e6bc18fbef0f8
CodeWasp942 <tmz50301@163.com>
2021-06-01 10:00:30 +0800

    modify cmake settings and CMakeLists.txt

--- example/demo.cpp
+++ example/demo.cpp
@@ -1,6 +1,14 @@
 #include "screen.h"
-using namespace FoxLab;
+#include "graph.h"
+#include <Windows.h>
+using namespace flab;
 int main(int argc, char** argv) {
-	screen scr;
-	return 0;
+	Fl_Window* window = new Fl_Window(500, 500, "111");
+	window->end();
+
+	Fl_Widget* wid = new mw(0,0,150,150);
+	window->add(wid);
+	window->show();
+	graph g;
+	return Fl::run();
 }
