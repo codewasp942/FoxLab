b9c2b812eda22b39ca9c1a18768a90863001d421
CodeWasp942 <tmz50301@163.com>
2021-06-01 14:21:53 +0800

    add monolithic header (don't recommend to use it)

---
+++ include/foxlab.h
@@ -0,0 +1,12 @@
+#pragma once
+
+/**
+* @file foxlab.h
+* @license GNU General Public License (GPL)
+* @brief This header includes all of the headers in FoxLab project.
+* @mainpage <h1>FoxLab arithmetic visualization library</h1>
+* ---
+* ## git repositories:
+* [github](https://github.com/codewasp942/FoxLab)
+* [gitee[
+*/
