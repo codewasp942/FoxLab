28e509be6f2bd3052ac523bacf6a6ddced61af70
CodeWasp942 <tmz50301@163.com>
2021-07-25 19:31:26 +0800

    modify demo

--- example/demo.cpp
+++ example/demo.cpp
@@ -6,13 +6,12 @@ int main(int argc, char** argv) {
 	srand(time(NULL));
 	screen scr;
 	scr.init(1300, 600);
-	scr.fps(1000);
+	scr.fps(100);
 	graph g(0, 0, 900, 600);
 	scr.add(&g);
 
 	for (int i = 0; i < 10; i++) {
 		g.addPoint();
-		scr.wait(100);
 	}
 	g.addEdge(3, 1);
 	g.addEdge(3, 2);
@@ -20,6 +19,11 @@ int main(int argc, char** argv) {
 	g.addEdge(5, 6);
 	g.addEdge(6, 7);
 	g.addEdge(3, 7);
+	scr.wait(500);
+
+	g.addPoint();
+	scr.wait(500);
+	g.addEdge(0, 10);
 	scr.wait(500);
 
 	return scr.runTillEnd();
